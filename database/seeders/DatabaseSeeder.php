d198f77b39a6a15b9bdb9cc2776db3885acc392b
Mikael <mikaelel902@gmail.com>
2025-05-31 20:24:07 +0700

    update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,14 +4,20 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Admin;
 
 class DatabaseSeeder extends Seeder
+
 {
     /**
      * Seed the application's database.
      */
+
+    
+
     public function run(): void
     {
+        Admin::factory()->count(10)->create();
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
