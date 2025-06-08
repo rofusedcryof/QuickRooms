ba126bfd60c1ed7478fceb045054108299da8d9a
rofusedcryof <dezttroll@gmail.com>
2025-06-08 10:28:19 +0700

    melengkapi program untuk class pemabayaran

--- app/Models/Pembayaran.php
+++ app/Models/Pembayaran.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Pembayaran extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'id_pembayaran';
+    protected $fillable = [
+        'id_pemesanan',
+        'metode_pembayaran',
+        'jumlah',
+        'status',
+        'tanggal_pembayaran',
+        'bukti_pembayaran',
+    ];
+
+    //relasi dgn class pemesanan
+    public function pemesanan()
+    {
+        return $this->belongsTo(Pemesanan::class, 'id_pemesanan');
+    }
 }
