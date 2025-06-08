8829158205e3bf87311f574f95b30320c37583bf
rofusedcryof <dezttroll@gmail.com>
2025-06-08 12:24:52 +0700

    menambah dan melengkapi tabel

--- database/migrations/2025_05_29_032821_create_pelanggans_table.php
+++ database/migrations/2025_05_29_032821_create_pelanggans_table.php
@@ -9,17 +9,30 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('pelanggans', function (Blueprint $table) {
-            $table->unsignedBigInteger('id_pelanggan', true); // true = auto-increment
-            $table->string('nama');
-            $table->string('email')->unique();
-            $table->string('telepon');
+            $table->id('id_pelanggan');//auto increment
+            $table->unsignedBigInteger('id_user')->nullable(); // relasi dgn user
+            $table->string('nama', 100);
+            $table->string('email', 100)->unique();
+            $table->string('no_hp', 20)->unique();
             $table->text('alamat');
             $table->timestamps();
+
+            // Foreign key constraint
+            $table->foreign('id_user')
+                ->references('id_user')->on('users')
+                ->onDelete('set null') // jika user dihapus, id_user di tabel ini akan diset null
+                ->onUpdate('cascade'); // jika id_user di tabel users diupdate, akan terupdate juga di sini
         });
     }
 
     public function down(): void
     {
+        Schema::table('pelanggans', function (Blueprint $table) {
+            // Hapus foreign key constraint sebelum menghapus tabel
+            $table->dropForeign(['id_user']);
+        });
+
+         // Hapus tabel pelanggans
         Schema::dropIfExists('pelanggans');
     }
 };
