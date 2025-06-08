f2e43e4049c29f30b55ef6c98ad7f41f5239564d
rofusedcryof <dezttroll@gmail.com>
2025-06-08 10:32:20 +0700

    melengkapi program untuk class ini

--- app/Models/tipe_kamar.php
+++ app/Models/tipe_kamar.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class tipe_kamar extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'id_tipe_kamar';
+    protected $fillable = [
+        'nama_tipe',
+        'deskripsi',
+        'harga',
+        'kapasitas',
+        'fasilitas',
+        'foto',
+    ];
+
+    //relasi dgn class pemesanan
+    public function pemesanans()
+    {
+        return $this->hasMany(Pemesanan::class, 'id_tipe_kamar');
+    }
 }
