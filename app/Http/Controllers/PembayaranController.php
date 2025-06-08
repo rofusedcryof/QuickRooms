fcf7ab9848da2ff07002e79b5785740364ce984c
Agustinus Kevinu <agustinuskevinyudipratams@gmail.com>
2025-06-08 17:56:25 +0700

    menambahkan controller Pemesanan, Pembayaran, TipeKamar

---
+++ app/Http/Controllers/PembayaranController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Pemesanan;
+use App\Models\Pembayaran;
+
+class PembayaranController extends Controller
+{
+    public function index()
+    {
+        
+    }
+
+    public function create()
+    {
+        $pemesanan = Pemesanan::all();// ambil semua data pemesanan
+        return view('pembayaran.create', compact('pemesanan'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'id_pemesanan' => 'required|exists:pemesanans,id_pemesanan',
+            'metode_pembayaran' => 'required|string|max:255',
+            'jumlah_bayar' => 'required|numeric|min:0',
+            'status_pembayaran' => 'required|string|max:255',
+        ]);
+
+        Pembayaran::create($request->all());
+        return redirect()->route('pembayarans.index')->with('success', 'Pembayaran berhasil dibuat.');
+    }
+
+    public function show($id)
+    {
+        
+    }
+
+    public function edit($id)
+    {
+       
+    }
+
+    public function update(Request $request, $id)
+    {
+       
+    }
+
+    public function destroy($id)
+    {
+       
+    }
+
+    public function pembayaranPemesanan($id)
+    {
+       
+    }
+    public function pembayaranKonfirmasi($id)
+    {
+        $pembayaran = Pembayaran::findOrFail($id);
+        $pembayaran->update(['status_pembayaran' => 'Terkonfirmasi']);
+        return redirect()->route('pembayarans.index')->with('success', 'Pembayaran berhasil dikonfirmasi.');
+    }
+}
---
+++ app/Http/Controllers/PemesananController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PemesananController extends Controller
+{
+    public function index()
+    {
+        $pemesanan = Pemesanan::all(); 
+        return view('pemesanan.index', compact('pemesanan')); 
+    }
+
+    public function create()
+    {
+        return view('pemesanan.create');
+    }
+
+    public function store(Request $request)
+    {
+        
+    }
+
+    public function show($id)
+    {
+        $pemesanan = Pemesanan::findOrFail($id); 
+        return view('pemesanan.show', compact('pemesanan'));
+    }
+
+    public function edit($id)
+    {
+        
+    }
+
+    public function update(Request $request, $id)
+    {
+        
+    }
+
+    public function destroy($id)
+    {
+        $pemesanan = Pemesanan::findOrFail($id);
+        $pemesanan->delete(); 
+        return redirect()->route('pemesanan.index')->with('success', 'Pemesanan deleted successfully');
+    }
+}
