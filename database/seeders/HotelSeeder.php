4adc4bcdba4cc39bb89bdc74842fe634ca354f43
Mikael <mikaelel902@gmail.com>
2025-06-17 16:39:31 +0700

    memperbaiki factory dari tingkat_kamar dan tipe kamar. menginput data pada table hotel, tipe_kamar dan tingkat_kamar

--- database/seeders/HotelSeeder.php
+++ database/seeders/HotelSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\tingkat_kamar;
+use App\Models\tipe_kamar;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,5 +16,23 @@ class HotelSeeder extends Seeder
     public function run(): void
     {
         hotel::factory(10)->create();
+
+        tipe_kamar::create([
+            'jenis' => 'single',
+            'deskripsi' => 'asdvafsvervarcadc',
+            'kapasitas' => 4,
+            'harga' => 120000,
+            'fasilitas' => 'Gym',
+            'foto' => 'hehe.jpeg',
+            'id_hotel' => 4
+        ]);
+
+        tingkat_kamar::create([
+            'nomor_kamar' => 107,
+            'lantai' => 1,
+            'status' => 'VIP',
+            'id_tipe' => 1,
+            'id_hotel' => 6
+        ]);
     }
 }
